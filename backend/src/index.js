d6b5f165d092e95f9b0558f4ae4d7924b630c2ec
Rian Santos <rianinacio@gmail.com>
2020-03-28 18:29:07 -0300

    Adicionada biblioteca celebrate para validação e Corrigido problema de segurança.

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const routes = require('./routes');
 const cors = require('cors');
 const app = express();
+const { errors } = require('celebrate');
 
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 /**
  * Métodos HTTP
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const routes = express.Router();
 
@@ -11,7 +12,15 @@ routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
 
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required().min(2),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required().min(10).max(11),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2),
+    })
+}), OngController.create);
 
 routes.get('/profile', ProfileController.index);
 
